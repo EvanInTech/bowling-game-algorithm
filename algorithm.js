7a0972da36030305585780cff490d2a17be60936
Evan Williams <79871203+EvanInTech@users.noreply.github.com>
2025-04-13 18:05:45 -0500

    Update algorithm.js
    loop through rolls and calculate score

--- algorithm.js
+++ algorithm.js
@@ -1,10 +1,24 @@
-const = rolls = [4, 3, 7, 5, 2, 8, 1, 4, 6, 2, 4, 8, 0, 8, 0, 8, 2, 10, 1, 7]; // 110
+const rolls = [4, 3, 7, 5, 2, 8, 1, 4, 6, 2, 4, 8, 0, 8, 0, 8, 2, 10, 1, 7]; // 110
 
 function calculateBowlingScore(rolls){
-let score = 0;
+  let score = 0;
+  let frames = 0;
 
+  for(let i = 0; i < rolls.length; i++){
+    if(rolls[i] === 10){ // strike
+      score += 10 + (rolls[i + 1] + rolls[i +2]);
+      frames++;
+    }
+    else if((rolls[i] + rolls[i + 1]) === 10){ // spare
+      score += 10 + rolls[i + 1];
+      frames++;
+    }else{
+      score += rolls[i] + rolls[i + 1];
+      frames++;
+    }
 
-
+    if(frames === 10) break;
+  }
 
   return score;
 }
