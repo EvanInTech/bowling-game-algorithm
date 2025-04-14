ba0ea06c680473cb80e56681c598380abdbae5fc
Evan Williams <79871203+EvanInTech@users.noreply.github.com>
2025-04-14 10:36:52 -0500

    Update algorithm.js
    use for loop for frames instead of rolls. iterate through rolls manually using roll index

--- algorithm.js
+++ algorithm.js
@@ -2,23 +2,24 @@ const rolls = [4, 3, 7, 5, 2, 8, 1, 4, 6, 2, 4, 8, 0, 8, 0, 8, 2, 10, 1, 7]; // 
 
 function calculateBowlingScore(rolls){
   let score = 0;
-  let frames = 0;
+  let rollIndex = 0;
 
-  for(let i = 0; i < rolls.length; i++){
-    if(rolls[i] === 10){ // strike
-      score += 10 + (rolls[i + 1] + rolls[i +2]);
-      frames++;
+  for(let i = 0; i < 10; i++){
+    if(rolls[rollIndex] === 10){ // strike
+      score += 10 + (rolls[rollIndex + 1] + rolls[rollIndex + 2]);
+      rollIndex += 1;
     }
-    else if((rolls[i] + rolls[i + 1]) === 10){ // spare
-      score += 10 + rolls[i + 1];
-      frames++;
+    else if((rolls[rollIndex] + rolls[rollIndex + 1]) === 10){ // spare
+      score += 10 + rolls[rollIndex + 2];
+      rollIndex += 2;
     }else{
-      score += rolls[i] + rolls[i + 1];
-      frames++;
+      score += rolls[rollIndex] + rolls[rollIndex + 1];
+      rollIndex += 2;
     }
-
-    if(frames === 10) break;
   }
 
   return score;
 }
+
+
+console.log(calculateBowlingScore(rolls));
